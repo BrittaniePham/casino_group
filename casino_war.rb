dfcc11c82b2b59975867f628311ba62fc5ce0246
Brittanie Pham <brittaniepham@gmail.com>
2018-05-11 14:22:04 -0600

    casino war: compare cards

--- casino_war.rb
+++ casino_war.rb
@@ -3,6 +3,8 @@ require_relative 'cards'
 class CasinoWar
 
   def initialize #bet
+    @wallet = 100
+    @bet = 10
     welcome
     puts "*cards are shuffled*"
     draw_2_cards
@@ -17,31 +19,42 @@ class CasinoWar
     elsif choice == 0 
       surrender
     else 
-      puts "Not a valid option"
+      puts "Not a valid option, try again"
+      tie
     end
   end
 
-  def surrender
+  def surrender() #bet parameter
+    # @half = @bet / 2
+    # total = @wallet - @half
   end
 
   def go_to_war
   end
 
   def win
-    puts "Your #{player_card} is greater than the dealer's #{dealer_card}"
+    puts "Your #{@player_card.rank} of #{@player_card.suit} is greater than the dealer's #{@dealer_card.rank} of #{@dealer_card.suit}"
     puts "Congrats! You won this round"
     #TODO WALLET + BET
     puts "You currently have ... " #TODO PRINT AMOUNT IN WALLET
   end
 
   def lose
+    puts "Your #{@player_card.rank} of #{@player_card.suit} is less than the dealer's #{@dealer_card.rank} of #{@dealer_card.suit}"
+    puts "Sorry! You lost this round"
+    #TODO WALLET - BET
+    puts "You currently have ... " #TODO PRINT AMOUNT IN WALLET
   end
 
   def compare_cards
-    if @player_card > @dealer_card
+    value 
+
+    if (@player_card.rank.to_i > @dealer_card.rank.to_i)
       win
-    else 
+    elsif (@player_card.rank.to_i < @dealer_card.rank.to_i)
       lose
+    elsif (@player_card.rank.to_i == @dealer_card.rank.to_i)
+      tie
     end
   end
 
@@ -56,8 +69,32 @@ class CasinoWar
     @dealer_card = deck1.cards.shuffle.sample
 
     puts "You received a #{@player_card.rank} of #{@player_card.suit} (#{@player_card.color})"
-   puts "Dealer received a #{@dealer_card.rank} of #{@dealer_card.suit} (#{@dealer_card.color})"
+    puts "Dealer received a #{@dealer_card.rank} of #{@dealer_card.suit} (#{@dealer_card.color})"
+    puts
+    compare_cards
   end
+
+  def value
+    case 
+    when @player_card.rank == 'A'
+      @player_card.rank = 1 
+    when @dealer_card.rank == 'A'
+      @dealer_card.rank = 1 
+    when @player_card.rank == 'J'
+      @player_card.rank = 11 
+    when @dealer_card.rank == 'J'
+      @dealer_card.rank = 11
+    when @player_card.rank == 'Q'
+      @player_card.rank = 12
+    when @dealer_card.rank == 'Q'
+      @dealer_card.rank = 12
+    when @player_card.rank == 'K'
+      @player_card.rank = 13
+    when @dealer_card.rank == 'K'
+      @dealer_card.rank = 13
+    end
+  end
+
 end
 
 start = CasinoWar.new
