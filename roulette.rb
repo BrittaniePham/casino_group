e37d62bcacd9e33ebe96aa94f76cddc1262088f9
Alex Winitzky <alex.winitzky@gmail.com>
2018-05-11 16:05:15 -0600

    roulette with bet function

--- roulette.rb
+++ roulette.rb
@@ -1,8 +1,10 @@
 #require 'pry'
 
 class Roulette
+attr_accessor :place_bet
 
-def initialize
+def initialize(bet)
+  @bet = bet
   @index = [0, 00, 2, 4, 7, 9, 10, 12, 15, 17, 18, 20, 23, 25, 26, 28, 31, 33, 34, 36, 1, 3, 5, 6, 8, 11, 13, 14, 16, 19, 21, 22, 24, 27, 29, 30, 32, 35]
   @color = ['Red', 'Black']
   play
@@ -15,7 +17,7 @@ def play
   puts "Try your luck, spin the wheel, and prepare for big wins!"
   puts "Or maybe you'll lose it all because of your poor life choices!"
   puts
-  puts '========================================'
+  puts '========================================='
   place_bet
 end
 
@@ -32,26 +34,29 @@ def place_bet
   else
     number
   end
-  color
- end
+    color
+end
 
- def number
-    puts "Pick a number between 1 and 36, in roulette you can also pick 0 or 00!"
+def number
+    puts "In Roulette you can pick a number between 1 and 36, 0(zero), or 00(double zero)"
     num_bet = gets.to_i
   if num_bet == @index.sample
     puts "Incredible, you win! You are the roulette master!"
+    @bet += @bet
   else
     puts "Ouch wah-wah! You have lost everything!"
- end
+    @bet -= @bet
+  end
 end
 
 
- def color
+def color
   if @colorchoice == @color.sample
     puts "You're a winner!"
-    return 50
+    @bet += @bet
   else
     puts "You have lost pretty horrifically, sorry."
+    @bet -= @bet
   end
 end
 
