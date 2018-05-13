c8c8cfcba262f8f08c1a244e24729f2ea784a724
Alex Winitzky <alex.winitzky@gmail.com>
2018-05-12 20:01:11 -0600

    smoother roulette features

--- roulette.rb
+++ roulette.rb
@@ -1,7 +1,7 @@
 #require 'pry'
 
 class Roulette
-attr_accessor :place_bet
+attr_accessor :bet
 
 def initialize(bet)
   @bet = bet
@@ -39,35 +39,63 @@ def place_bet
 end
 
 def number
-    puts "In Roulette you can pick a number between 1 and 36, 0(zero), or 00(double zero)"
-    num_bet = gets.to_i
-  if num_bet == @index.sample
-    puts "Incredible, you win! You are the roulette master!"
-    @bet = @bet + 10
-    @player_money = @player_money + @bet
-    puts "You just won 10 moneys!"
+  puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+  puts "In Roulette you can pick a single number between 1 and 36, 0 (zero), or 00 (double zero)"
+  puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+  puts "The odds really suck for you when you do this but ok, what's your pick?"
+  print "> "
+  @num_bet = gets.to_i
+  if @num_bet == @index.sample
+    num_win
   else
-    puts "Ouch wah-wah!"
-    @bet = @bet - 10
-    @player_money = @player_money - @bet
-    puts "You just lost 10 money units! That sucks!"
+    num_lose
   end
 end
 
+def num_win
+  @bet = @bet * 3
+  @player_money = @player_money + @bet
+  puts "$$$$ CASH $$$$$$ CASH $$$$$$ CASH $$$$$$ CASH $$$$"
+  puts "Incredible, you win! You are the roulette master!"
+  puts "Make it Rain!! You just tripled your cash!!"
+  puts "You you're up to #{@bet}!"
+  puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+  puts "You're on a streak!"
+  puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+end
+
+def num_lose
+  @bet = @bet / 3
+  @player_money = @player_money - @bet
+  puts "* :( * * :( * * :( * * :( * * :( * * :( *"
+  puts "Yikes, didn't I tell you?"
+  puts "You have lost pretty horrifically."
+  puts "Like, damn... You're down to #{@bet}."
+  puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+  puts "Better luck next time."
+  puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+end
 
 def color
   if @colorchoice == @color.sample
-    puts "You're a winner!"
-    @bet = @bet * 3
+    @bet = @bet + 20
     @player_money = @player_money + @bet
-    puts "Whoa!! Make it Rain!! You just tripled your cash!!"
+    puts "$$$$$ CASH $$$$$$ CASH $$$$$$ CASH $$$$$$ CASH $$$$$$ CASH $$$$$$ CASH $$$$$"
+    puts "Cha-Ching!  You're a winner!"
+    puts "You just won 20 moneys! You're up to #{@bet}!"
+    puts "Just watch out for the mob, sometimes they break your legs when you win haha!"
+    puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
   else
-    puts "You have lost pretty horrifically, sorry."
-    @bet = @bet / 3
+    @bet = @bet - 20
     @player_money = @player_money - @bet
-    puts "Like, damn.  You just lost 2/3 of what you had."
+    puts "* * :( * * :( * * :( * * :( * * :( * * :( * * :( * * :( * * :( * * :( * *"
+    puts "Ouch! You just lost 20 currency units! That sucks!"
+    puts "You're down to #{@bet}."
+    puts "Well onward and upward, I guess."
+    puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
   end
 end
 
-
 end
+
+#Roulette.new(1)
